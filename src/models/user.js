50d2f6304228ecc1085de74987887236e3fd37b4
Ayush Singh <ayushsops@gmail.com>
2022-02-22 19:54:19 +0530

    init

---
+++ src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if(!validator.isEmail(value)) {
+        throw new Error('Invalid Email!')
+      }
+    }
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if(value<0) {
+        throw new Error('Age must be a +ve number!')
+      }
+    }
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    validate(value) {
+      if(!validator.isLength(value, {min: 6})) {
+          throw new Error('Password Too Short!')
+      }
+      if(value.toLowerCase().includes("password")) {
+        throw new Error("Password cannot contain the string 'password'.")
+      }
+    }
+  }
+})
+
+module.exports = User
