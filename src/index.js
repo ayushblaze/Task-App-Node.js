e50338c132e71bf5b81ec77cfa92c78dada8f0fa
Ayush Singh <ayushsops@gmail.com>
2022-02-27 17:00:43 +0530

    Logging in user, jwt

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path)
+  next()
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -15,6 +20,18 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server Running on Port '+port)
 })
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+  const token = jwt.sign({ _id:'abc123' }, 'thisismycourse', { expiresIn: '5 sec' })
+  console.log(token)
+
+  const data = jwt.verify(token, 'thisismycourse')
+  console.log(data)
+}
+
+myFunction()
 
 // const bcrypt = require('bcrypt')
 
