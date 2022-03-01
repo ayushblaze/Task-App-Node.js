6c0b3a1202b3991fe79bbe0d1b710a1846d615ec
Ayush Singh <ayushsops@gmail.com>
2022-03-01 13:14:13 +0530

    logging in

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-  console.log(req.method, req.path)
-  next()
-})
+// app.use((req, res, next) => {
+//   if(req.method === 'GET') {
+//     res.send('GET requests are disabled')
+//   } else {
+//     next()
+//   }
+// })
+
+// app.use((req, res, next) => {
+//   if(req.method) {
+//     res.status(503).send('The site is under maintaince :(')
+//   } else {
+//     next()
+//   }
+// })
 
 app.use(express.json())
 app.use(userRouter)
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -28,13 +29,26 @@ router.post('/users/login', async(req, res) => {
   }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth, async(req, res) => {
   try {
-    const users = await User.find({})
-    res.status(201).send(users)
-  } catch(e) {
-    res.status(400).send(e)
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+    await req.user.save()
+    res.send()
+  } catch (e) {
+    res.status(500).send()
   }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user)
+  // try {
+  //   const users = await User.find({})
+  //   res.send(users)
+  // } catch(e) {
+  //   res.status(500).send()
+  // }
   // User.find({}).then((users) => {
   //   res.send(users)
   // }).catch((e) => {
